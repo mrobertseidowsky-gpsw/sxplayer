ba3a4d9fe8492166cd6ffb65121ce72ecc2280f2
mrobertseidowsky <mrobertseidowsky@gopro.com>
2020-10-28 13:56:52 +0100

    test win

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, Meson
+
+class HelloConan(ConanFile):
+    name = "sxplayer"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "pkg_config"
+    exports_sources = "src/*"
+    requires = "zlib/1.2.11"
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure(source_folder="./",
+                        build_folder="build")
+        meson.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src=".")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["sxplayer"]
