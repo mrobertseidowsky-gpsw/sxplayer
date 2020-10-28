790560a06d66936bb5cf3ea8fed1fdb99018178f
mrobertseidowsky <mrobertseidowsky@gopro.com>
2020-10-28 15:25:56 +0100

    test win

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class HelloConan(ConanFile):
 
     def build(self):
         meson = Meson(self)
-        meson.configure(build_folder="build")
+        meson.configure(build_folder=".")
         meson.build()
 
     def package(self):
