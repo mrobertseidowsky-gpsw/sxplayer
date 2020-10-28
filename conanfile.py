2b388eeadfac56368a36a8abde3937fe8f90ff3e
mrobertseidowsky <mrobertseidowsky@gopro.com>
2020-10-28 14:17:26 +0100

    test win

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class HelloConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "pkg_config"
     exports_sources = "src/*"
-    requires = "libavfilter"
+    requires = "ffmpeg/4.2@bincrafters/stable"
 
     def build(self):
         meson = Meson(self)
